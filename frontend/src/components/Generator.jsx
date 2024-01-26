64f0f1f813d285a4a429e5eff4680ba72c7b4b16
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 19:31:43 +0100

    Implemented code for Generator compoent

--- frontend/src/components/Generator.jsx
+++ frontend/src/components/Generator.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { FaDiceFive } from 'react-icons/fa6';
 
 import dividerMobileImg from './../assets/pattern-divider-mobile.svg';
@@ -6,41 +5,23 @@ import dividerDesktopImg from './../assets/pattern-divider-desktop.svg';
 
 import './generator.scss';
 
-const Generator = () => {
-  const [currentAdvice, setCurrentAdvice] = useState([
-    {
-      id: 117,
-      content: "I'm sorry, but I don't know what to do with that.",
-      generatedAt: new Date().toLocaleString(),
-    },
-  ]);
-
+const Generator = ({ advice }) => {
   return (
-    <div className="advice">
-      {currentAdvice.map((advice) => {
-        return (
-          <>
-            <p className="advice__number">Advice #{advice.id}</p>
-            <p className="advice__content">&ldquo;{advice.content}&rdquo;</p>
-            <picture class="advice__separator">
-              <source
-                media="(max-width: 900px)"
-                srcset={dividerMobileImg}
-                alt="Divider image for mobile version"
-              />
-              <img
-                src={dividerDesktopImg}
-                alt="Divider image for desktop version"
-                className="advice__separator-desktop-img"
-              />
-            </picture>
-            <button className="advice__generate-advice-btn">
-              <FaDiceFive className="advice__generate-advice-btn-icon" />
-            </button>
-          </>
-        );
-      })}
-    </div>
+    <>
+      <p className="advice__number">Advice #{advice.id}</p>
+      <p className="advice__content">&ldquo;{advice.content}&rdquo;</p>
+      <picture className="advice__separator">
+        <source
+          srcSet={dividerMobileImg}
+          media="(max-width: 900px)"
+          alt="Divider image for mobiles"
+        />
+        <img src={dividerDesktopImg} alt="Divider image for desktop" />
+      </picture>
+      <button className="advice__generate-btn">
+        <FaDiceFive className="advice__generate-icon" />
+      </button>
+    </>
   );
 };
 
