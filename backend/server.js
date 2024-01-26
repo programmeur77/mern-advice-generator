86098048dc018b4f4969ccbe913b5b4855976f8e
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 14:02:44 +0100

    Added urlEncorded, headers, userRoutes and database connection function

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,31 @@
 import dotenv from 'dotenv/config';
 import express from 'express';
 
+import userRoutes from './routes/user-routes.js';
+import dbConnect from './config/db.js';
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTION, PATCH, UPDATE'
+  );
+  next();
+});
 
 app.use(express.json());
 
+app.use('/api/users', userRoutes);
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port 3000');
+  dbConnect();
 });
