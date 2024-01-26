a239185f401d51d98ffee0829ce361f50d3f030a
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 14:49:34 +0100

    Added routes for advice update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv/config';
 import express from 'express';
 
 import userRoutes from './routes/user-routes.js';
+import adviceRoutes from './routes/advice-routes.js';
+
 import dbConnect from './config/db.js';
 
 const app = express();
@@ -24,6 +26,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/advice', adviceRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server running on port 3000');
