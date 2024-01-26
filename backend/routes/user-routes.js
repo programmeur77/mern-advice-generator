fac7eec90902d5ecc1d96c55b2f4bd3ac831c1dc
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 14:21:06 +0100

    Added POST route to login user

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { createUser } from './../controllers/user-controllers.js';
+import { createUser, loginUser } from './../controllers/user-controllers.js';
 
 const router = express.Router();
 
 router.post('/create', createUser);
+router.post('/login', loginUser);
 
 export default router;
