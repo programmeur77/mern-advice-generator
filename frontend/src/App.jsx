f51e9c797b47bafc68a09b588e32e85f9127f83b
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 23:54:19 +0100

    Implemented code logic without backend for the moment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,47 @@
+import { useState, useEffect } from 'react';
+
 import dividerDesktop from './assets/pattern-divider-desktop.svg';
 import dividerMobile from './assets/pattern-divider-mobile.svg';
 import iconDice from './assets/icon-dice.svg';
+import loader from './assets/loader.svg';
 
 import './App.scss';
 function App() {
-  const [currentAdvice, setCurrentAdvice] = useState([
-    {
-      id: 1,
-      content: "I'm sorry, but I don't know what to do with that.",
-      generatedAt: new Date().toLocaleString(),
-    },
-  ]);
+  const [currentAdvice, setCurrentAdvice] = useState(null);
+
+  const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    const data = await response.json();
+    setAdviceAndSave(data.slip);
+  };
+
+  const setAdviceAndSave = (advice) => {
+    setCurrentAdvice([advice]);
+    localStorage.setItem('advice', JSON.stringify(advice));
+  };
+
+  useEffect(() => {
+    if (!localStorage.getItem('advice')) {
+      if (fetchAdvice()) {
+        throw new Error();
+      }
+    }
+
+    setCurrentAdvice([JSON.parse(localStorage.getItem('advice'))]);
+  }, []);
 
   return (
-    <>
-      <div className="generator-container">
-        {currentAdvice.map((advice) => {
+    <div className="generator-container">
+      {currentAdvice &&
+        currentAdvice.map((advice) => {
           return (
             <>
               <p className="advice__number">Advice #{advice.id}</p>
-              <p className="advice__content">&ldquo;{advice.content}&rdquo;</p>
+              <p className="advice__content">&ldquo;{advice.advice}&rdquo;</p>
               <picture className="advice__separator">
                 <source
                   media="(max-width: 900px)"
-                  srcset={dividerMobile}
+                  srcSet={dividerMobile}
                   alt="Divider image for mobile"
                 />
                 <img src={dividerDesktop} alt="Divider image for desktop" />
@@ -38,8 +56,7 @@ function App() {
             </>
           );
         })}
-      </div>
-    </>
+    </div>
   );
 }
 
