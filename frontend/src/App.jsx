cd85ff9b6a3de3cf8b8e769a86cd1bf434580097
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 19:29:23 +0100

    Added a container and a mapping of state and import Generato component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import Generator from './components/Generator';
+import { useState } from 'react';
 
+import './app.scss';
+
+import Generator from './components/Generator';
 const App = () => {
+  const [currentAdvice, setCurrentAdvice] = useState([
+    {
+      id: 117,
+      content: "I'm sorry, but I don't know what to do with that.",
+      generatedAt: new Date().toLocaleString(),
+    },
+  ]);
+
   return (
     <div className="generator-container">
-      <Generator />
+      {currentAdvice.map((advice) => {
+        return <Generator advice={advice} key={advice.id} />;
+      })}
     </div>
   );
 };
