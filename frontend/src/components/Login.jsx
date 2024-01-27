6c2077df1bb92e75a126de07b6faec33a8e981cf
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 00:04:40 +0100

    Implemented Login container for login form

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import LoginForm from './LoginForm';
+
+import './Login.scss';
+
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <div className="login">
+      <h1 className="login__form-title">Login</h1>
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
