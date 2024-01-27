68b5d93eb1b1d9f159bc567d34dfced65775569e
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 00:06:54 +0100

    Implemented JSX code and CSS style for login form component

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { IoMdEye } from 'react-icons/io';
+import { FiLogIn } from 'react-icons/fi';
+
+import './LoginForm.scss';
+
+const LoginForm = () => {
+  return (
+    <>
+      <form action="POST" className="login-form">
+        <input
+          type="text"
+          name="email"
+          className="login-form__email-input"
+          placeholder="example@example.com"
+          autoFocus
+        />
+        <div className="login-form__password-container">
+          <input
+            type="password"
+            name="password"
+            className="login-form__password-input"
+            placeholder="Password"
+          />
+          <IoMdEye className="login-form__eye-icon" />
+        </div>
+        <p className="login-form__error">Error</p>
+        <button className="login-form__submit-btn">
+          <FiLogIn className="login-form__btn-icon" />
+        </button>
+      </form>
+
+      <div className="login-form__link-section">
+        <Link
+          to="/forgotten-password"
+          className="login-form__forgotten-password-link"
+        >
+          Forgotten password
+        </Link>
+
+        <p className="login-form__no-account-text">
+          Don't have an account yet ?
+          <Link to="/register" className="login-form__register-link">
+            &nbsp;Register
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
