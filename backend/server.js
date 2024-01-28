478e829d8fd2d533bac48daca2e93536fe23e618
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 15:57:44 +0100

    Added cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 import userRoutes from './routes/user-routes.js';
 import adviceRoutes from './routes/advice-routes.js';
@@ -10,20 +11,8 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTION, PATCH, UPDATE'
-  );
-  next();
-});
-
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/advice', adviceRoutes);
