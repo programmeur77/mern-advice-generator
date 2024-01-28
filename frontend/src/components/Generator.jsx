947a9c0e33807c81f12c8c4fb099486aed0dfd10
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 20:30:16 +0100

    Deleted all code to restart to fix bugs - wrote functions to generate a new advice

--- frontend/src/components/Generator.jsx
+++ frontend/src/components/Generator.jsx
@@ -4,88 +4,41 @@ import { useNavigate } from 'react-router-dom';
 
 import Advice from './Advice';
 
-const Generator = ({ user, userId }) => {
-  const [currentAdvice, setCurrentAdvice] = useState(null);
+const Generator = ({
+  user,
+  setUser,
+  userId,
+  currentAdvice,
+  setCurrentAdvice,
+}) => {
   const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     user === null ? navigate('/login') : null;
-
-    currentAdvice === null ? generateAdviceAndStore() : null;
+    currentAdvice === null ? generateAdvice() : null;
   }, []);
 
-  useEffect(() => {
-    currentAdvice !== null ? createNewAdvice(currentAdvice) : console.log('no');
-  }, [currentAdvice]);
-
   const fetchAdvice = async () => {
-    setIsLoading(true);
     try {
-      const response = await fetch('https://api.adviceslip.com/advice');
-
-      if (response.ok) {
-        const data = await response.json();
-        setIsLoading(false);
-        return data.slip;
-      }
+      const adviceResponse = await fetch('https://api.adviceslip.com/advice');
+      if (!adviceResponse.ok) throw new Error('Could not fetch advice');
+      const advice = await adviceResponse.json();
+      return advice;
     } catch (error) {
       console.log(error);
     }
   };
 
-  const fetchCreateAdvice = async (advice) => {
-    const response = await fetch(
-      `http://localhost:3000/api/advice/${userId}/updateAdvice`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(advice),
-      }
-    );
-
+  const generateAdvice = async () => {
     try {
-      if (!response.ok) {
-        throw new Error('Failed to create advice');
-      }
-      const data = await response.json();
-      return data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const createNewAdvice = (currentAdvice) => {
-    currentAdvice.map((advice) => {
-      const newAdvice = {
-        adviceId: advice.id,
-        advice: advice.advice,
-        generatedAt: new Date().toLocaleString(),
-      };
-      saveAdvice(newAdvice);
-    });
-  };
-
-  const saveAdvice = async (advice) => {
-    try {
-      const adviceResponse = await fetchCreateAdvice(advice);
-      if (!adviceResponse) {
-        throw new Error('Failed to save advice');
-      }
-      console.log(adviceResponse);
+      const newAdvice = await fetchAdvice();
+      if (!newAdvice) throw new Error('Error in fetching advice');
+      console.log(newAdvice);
     } catch (error) {
       console.log(error);
     }
-  };
-
-  const generateAdviceAndStore = async () => {
-    const advice = await fetchAdvice();
-
-    setCurrentAdvice([advice]);
-    localStorage.setItem('advice', JSON.stringify(advice));
   };
 
   return (
