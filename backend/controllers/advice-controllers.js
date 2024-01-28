71623cdbb3b851ce2e0cbcb7cb32c1ba843d556f
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 19:45:38 +0100

    Re-implemented advice Update function to make it work

--- backend/controllers/advice-controllers.js
+++ backend/controllers/advice-controllers.js
@@ -2,28 +2,20 @@ import User from './../models/user-model.js';
 
 export const updateAdvice = async (req, res) => {
   const { userId } = req.params;
-  const { adviceId, content, generatedAt } = req.body;
+  const { adviceId, advice, generatedAt } = req.body;
 
   const updatedAdvice = await User.findOneAndUpdate(
     { _id: userId },
-    {
-      $set: {
-        advice: { id: adviceId, content: content, generatedAt: generatedAt },
-      },
-    }
+    { advice: { id: adviceId, advice: advice, generatedAt: generatedAt } }
   );
 
   if (!updatedAdvice) {
-    return res
-      .status(400)
-      .json({ error: 'Could not update advice', status: 400 });
+    res.status(400).json({ error: 'Unable ton find and update' });
   }
 
-  res
-    .status(200)
-    .json({
-      updatedAdvice: updatedAdvice,
-      success: 'Advice updated successfully',
-      status: 200,
-    });
+  res.status(201).json({
+    updatedAdvice: updatedAdvice,
+    message: 'Advice updated successfully',
+    status: 201,
+  });
 };
