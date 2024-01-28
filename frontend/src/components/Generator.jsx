8499705e8891b2ab8331bfe4d4a1b09dca0212c7
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 19:50:11 +0100

    Moved in the component most of the code that was in the App.jsx component an that created bugs

--- frontend/src/components/Generator.jsx
+++ frontend/src/components/Generator.jsx
@@ -4,18 +4,88 @@ import { useNavigate } from 'react-router-dom';
 
 import Advice from './Advice';
 
-const Generator = ({ currentAdvice, fetchAdvice, isLoading }) => {
+const Generator = ({ user, userId }) => {
+  const [currentAdvice, setCurrentAdvice] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    const user = localStorage.getItem('user');
-    if (!user) navigate('/login');
+    user === null ? navigate('/login') : null;
+
+    currentAdvice === null ? generateAdviceAndStore() : null;
   }, []);
-  const handleOnClick = (e) => {
-    e.preventDefault();
-    if (!fetchAdvice()) {
-      throw new Error();
+
+  useEffect(() => {
+    currentAdvice !== null ? createNewAdvice(currentAdvice) : console.log('no');
+  }, [currentAdvice]);
+
+  const fetchAdvice = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+
+      if (response.ok) {
+        const data = await response.json();
+        setIsLoading(false);
+        return data.slip;
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  const fetchCreateAdvice = async (advice) => {
+    const response = await fetch(
+      `http://localhost:3000/api/advice/${userId}/updateAdvice`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(advice),
+      }
+    );
+
+    try {
+      if (!response.ok) {
+        throw new Error('Failed to create advice');
+      }
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const createNewAdvice = (currentAdvice) => {
+    currentAdvice.map((advice) => {
+      const newAdvice = {
+        adviceId: advice.id,
+        advice: advice.advice,
+        generatedAt: new Date().toLocaleString(),
+      };
+      saveAdvice(newAdvice);
+    });
+  };
+
+  const saveAdvice = async (advice) => {
+    try {
+      const adviceResponse = await fetchCreateAdvice(advice);
+      if (!adviceResponse) {
+        throw new Error('Failed to save advice');
+      }
+      console.log(adviceResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const generateAdviceAndStore = async () => {
+    const advice = await fetchAdvice();
+
+    setCurrentAdvice([advice]);
+    localStorage.setItem('advice', JSON.stringify(advice));
   };
 
   return (
@@ -27,7 +97,7 @@ const Generator = ({ currentAdvice, fetchAdvice, isLoading }) => {
               <Advice
                 advice={advice}
                 key={advice.id}
-                handleOnClick={handleOnClick}
+                // handleOnClick={handleOnClick}
                 isLoading={isLoading}
               />
             );
