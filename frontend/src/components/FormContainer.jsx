940efe0f06906958aca20ffce70664c2172e78ac
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-28 08:05:52 +0100

    Added conditionnal rendering according to location pathname

--- frontend/src/components/FormContainer.jsx
+++ frontend/src/components/FormContainer.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
 
 import './FormContainer.scss';
 
 const Login = () => {
+  const location = useLocation();
+  const [formTitle, setFormTitle] = useState('');
+
   return (
     <div className="form-container">
-      <h1 className="form-container__form-title">Login</h1>
-      <LoginForm />
+      <h1 className="form-container__form-title">{formTitle}</h1>
+      {location.pathname === '/login' ? (
+        <LoginForm setFormTitle={setFormTitle} />
+      ) : (
+        <SignupForm setFormTitle={setFormTitle} />
+      )}
     </div>
   );
 };
